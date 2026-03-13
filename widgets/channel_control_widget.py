43227eaed90579eb4da90001c5c917f92d05c67c
Abel Zambrana <abel.led94@gmail.com>
2026-03-13 17:47:52 -0300

    Se realiza modificación en controles de osciloscopio

--- widgets/channel_control_widget.py
+++ widgets/channel_control_widget.py
@@ -9,6 +9,7 @@ class ChannelControlWidget(QtWidgets.QGroupBox):
     Widget de control para un canal del osciloscopio.
     Incluye:
     - Selector de entrada
+    - Selector de amplitud
     - Botones DC / AC / GND (solo visuales por ahora)
     """
 
@@ -41,19 +42,67 @@ class ChannelControlWidget(QtWidgets.QGroupBox):
         # Entrada
         entry_row = QtWidgets.QHBoxLayout()
         entry_label = QtWidgets.QLabel("Entrada")
-        entry_combo = QtWidgets.QComboBox()
-        entry_combo.addItem("Ninguna")
-
+        self.entry_combo = QtWidgets.QComboBox()
+        self.entry_combo.addItems([
+            "Ninguna",
+            "E1",
+            "E2",
+            "E3",
+            "I1",
+            "I2",
+            "I3",
+            "P1",
+            "P2",
+            "P3",
+            "T",
+            "N",
+            "Pm",
+            "EA-1",
+            "EA-2",
+            "EA-3",
+            "EA-4",
+            "EA-5",
+            "EA-6",
+            "EA-7",
+            "EA-8"
+        ])
         entry_row.addWidget(entry_label)
-        entry_row.addWidget(entry_combo)
+        entry_row.addWidget(self.entry_combo)
         layout.addLayout(entry_row)
+        
+        # Lista desplegable de Amplitud por división
+        
+        volt_row = QtWidgets.QHBoxLayout()
+        volt_label = QtWidgets.QLabel("Amplitud")
+
+        self.volt_combo = QtWidgets.QComboBox()
+        volt_row.addWidget(volt_label)
+        volt_row.addWidget(self.volt_combo)
+        layout.addLayout(volt_row)
+
+        self.entry_combo.currentTextChanged.connect(self.update_amplitude_options)
+
 
         # Botones DC / AC / GND
         btn_row = QtWidgets.QHBoxLayout()
 
-        self.btn_dc = QtWidgets.QPushButton("─")
+        #self.btn_dc = QtWidgets.QPushButton("─")
+        self.btn_dc = QtWidgets.QPushButton("⎓")
         self.btn_ac = QtWidgets.QPushButton("∿")
         self.btn_gnd = QtWidgets.QPushButton("⏚")
+
+
+        for btn in (self.btn_dc, self.btn_ac, self.btn_gnd):
+            btn.setCheckable(True)
+        
+        self.coupling_group = QtWidgets.QButtonGroup(self)
+        self.coupling_group.setExclusive(True)
+
+        self.coupling_group.addButton(self.btn_dc)
+        self.coupling_group.addButton(self.btn_ac)
+        self.coupling_group.addButton(self.btn_gnd)
+
+        self.btn_dc.setChecked(True)
 
         for btn in (self.btn_dc, self.btn_ac, self.btn_gnd):
             btn.setFixedSize(26, 22)
@@ -72,6 +121,15 @@ class ChannelControlWidget(QtWidgets.QGroupBox):
                     border-bottom: 2px solid #ffffff;
                     border-right: 2px solid #ffffff;
                 }
+
+                QPushButton:checked {
+                    background: #c6c6c6;
+                    border-top: 2px solid #7a7a7a;
+                    border-left: 2px solid #7a7a7a;
+                    border-bottom: 2px solid #ffffff;
+                    border-right: 2px solid #ffffff;
+                }
+
                 """
             )
 
@@ -80,3 +138,72 @@ class ChannelControlWidget(QtWidgets.QGroupBox):
         btn_row.addWidget(self.btn_gnd)
 
         layout.addLayout(btn_row)
+
+
+    def update_amplitude_options(self, text):
+
+            self.volt_combo.clear()
+
+    # ---------------- VOLTAJE ----------------
+            if text.startswith("E") or text.startswith("EA"):
+                self.volt_combo.addItems([
+                    "2 V/div.",
+                    "5 V/div.",
+                    "10 V/div.",
+                    "20 V/div.",
+                    "50 V/div.",
+                    "100 V/div.",
+                    "200 V/div."
+                ])
+
+    # ---------------- CORRIENTE ----------------
+            elif text.startswith("I"):
+                self.volt_combo.addItems([
+                    "0.05 A/div.",
+                    "0.1 A/div.",
+                    "0.2 A/div.",
+                    "0.5 A/div.",
+                    "1 A/div.",
+                    "2 A/div.",
+                    "5 A/div."
+                ])
+
+    # ---------------- POTENCIA ----------------
+            elif text.startswith("P"):
+                self.volt_combo.addItems([
+                    "5 W/div.",
+                    "10 W/div.",
+                    "20 W/div.",
+                    "50 W/div.",
+                    "100 W/div.",
+                    "200 W/div.",
+                    "500 W/div."
+                ])
+
+    # ---------------- TEMPERATURA ----------------
+            elif text == "T":
+                self.volt_combo.addItems([
+                    "0.1 N.m/div.",
+                    "0.2 N.m/div.",
+                    "0.5 N.m/div.",
+                    "1 N.m/div.",
+                    "2 N.m/div.",
+                    "5 N.m/div.",
+                    "10 N.m/div."
+                ])
+
+    # ---------------- NEUTRO ----------------
+            elif text == "N":
+                self.volt_combo.addItems([
+                    "20 rpm/div.",
+                    "50 rpm/div.",
+                    "100 rpm/div.",
+                    "200 rpm/div.",
+                    "500 rpm/div.",
+                    "1000 rpm/div.",
+                    "2000 rpm/div."
+                ])
+
+    # ---------------- NINGUNA ----------------
+            else:
+                self.volt_combo.addItem("Sin señal")
--- widgets/oscilloscope_widget.py
+++ widgets/oscilloscope_widget.py
@@ -15,10 +15,11 @@ class OscilloscopeGrid(QtWidgets.QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setMinimumSize(520, 420)
-        self.setStyleSheet("background: #008b8b;")
+        #self.setStyleSheet("background: #008b8b;")
 
     def paintEvent(self, event):
         painter = QtGui.QPainter(self)
+        painter.fillRect(self.rect(), QtGui.QColor("#008b8b"))
         painter.setRenderHint(QtGui.QPainter.Antialiasing, False)
 
         w = self.width()
@@ -31,22 +32,9 @@ class OscilloscopeGrid(QtWidgets.QWidget):
         col_w = w / cols
         row_h = h / rows
 
-        # Líneas finas (subdivisiones)
-        pen_sub = QtGui.QPen(QtGui.QColor("#4fd1c5"))
-        pen_sub.setWidth(1)
-        painter.setPen(pen_sub)
-
-        for c in range(cols * sub + 1):
-            x = c * (w / (cols * sub))
-            painter.drawLine(int(x), 0, int(x), h)
-
-        for r in range(rows * sub + 1):
-            y = r * (h / (rows * sub))
-            painter.drawLine(0, int(y), w, int(y))
-
         # Líneas principales
         pen_main = QtGui.QPen(QtGui.QColor("#b2ffff"))
-        pen_main.setWidth(2)
+        pen_main.setWidth(0.5)
         painter.setPen(pen_main)
 
         for c in range(cols + 1):
@@ -57,13 +45,37 @@ class OscilloscopeGrid(QtWidgets.QWidget):
             y = r * row_h
             painter.drawLine(0, int(y), w, int(y))
 
-        # Cruz central
+        # Cruz central (líneas principales)
         pen_center = QtGui.QPen(QtGui.QColor("white"))
-        pen_center.setWidth(2)
+        pen_center.setWidth(1.5)
         painter.setPen(pen_center)
 
-        painter.drawLine(w // 2, 0, w // 2, h)
-        painter.drawLine(0, h // 2, w, h // 2)
+        cx = w // 2
+        cy = h // 2
+
+        painter.drawLine(cx, 0, cx, h)
+        painter.drawLine(0, cy, w, cy)
+
+        # Subdivisiones de la cruz central
+        pen_sub = QtGui.QPen(QtGui.QColor("white"))
+        pen_sub.setWidth(1)
+        painter.setPen(pen_sub)
+
+        subdivisions = 5            # cantidad de subdivisiones por división
+        tick_size = 4               # tamaño de cada marca
+        col_w = w / 10              # 10 divisiones horizontales
+        row_h = h / 8               # 8 divisiones verticales
+
+        # Subdivisiones horizontales sobre la línea vertical central
+        for i in range(1, 10 * subdivisions):
+            y = int(i * (row_h / subdivisions))
+            painter.drawLine(cx - tick_size, y, cx + tick_size, y)
+
+        # Subdivisiones verticales sobre la línea horizontal central
+        for i in range(1, 10 * subdivisions):
+            x = int(i * (col_w / subdivisions))
+            painter.drawLine(x, cy - tick_size, x, cy + tick_size)
+
 
 
 class OscilloscopeWidget(QtWidgets.QWidget):
@@ -92,14 +104,14 @@ class OscilloscopeWidget(QtWidgets.QWidget):
         channels_layout.setSpacing(6)
 
         channel_defs = [
-            ("Can1 (X)", QtGui.QColor("lime")),
+            ("Can1 (X)", QtGui.QColor("green")),
             ("Can2 (Y)", QtGui.QColor("yellow")),
-            ("Can3", QtGui.QColor("cyan")),
+            ("Can3", QtGui.QColor("violet")),
             ("Can4", QtGui.QColor("red")),
-            ("Can5", QtGui.QColor("white")),
-            ("Can6", QtGui.QColor("white")),
-            ("Can7", QtGui.QColor("white")),
-            ("Can8", QtGui.QColor("white")),
+            ("Can5", QtGui.QColor("orange")),
+            ("Can6", QtGui.QColor("blue")),
+            ("Can7", QtGui.QColor("black")),
+            ("Can8", QtGui.QColor("brown")),
         ]
 
         for i, (name, color) in enumerate(channel_defs):
@@ -118,7 +130,7 @@ class OscilloscopeWidget(QtWidgets.QWidget):
         data_group = QtWidgets.QGroupBox("Datos Formas de ondas")
         data_layout = QtWidgets.QVBoxLayout(data_group)
 
-        table = QtWidgets.QTableWidget(4, 4)
+        table = QtWidgets.QTableWidget(9, 4)
         table.setHorizontalHeaderLabels(["Cursores", "EFI", "PRO", "f (Hz)"])
         table.verticalHeader().setVisible(False)
         table.setStyleSheet(
@@ -130,7 +142,7 @@ class OscilloscopeWidget(QtWidgets.QWidget):
             """
         )
         data_layout.addWidget(table)
-        bottom_layout.addWidget(data_group, stretch=3)
+        bottom_layout.addWidget(data_group, stretch=1)
 
         # Derecha: base de tiempo
         time_group = QtWidgets.QGroupBox("Base de tiempo")
@@ -138,11 +150,22 @@ class OscilloscopeWidget(QtWidgets.QWidget):
 
         time_combo = QtWidgets.QComboBox()
         time_combo.addItems([
-            "0.2 ms/div",
-            "0.5 ms/div",
-            "1 ms/div",
-            "100 ms/div",
-            "1 s/div"
+            "0.2 ms/div.",
+            "0.5 ms/div.",
+            "1 ms/div.",
+            "1.66667 ms/div.",
+            "2 ms/div.",
+            "5 ms/div.",
+            "10 ms/div.",
+            "20 ms/div.",
+            "50 ms/div.",
+            "100 ms/div.",
+            "0.2 s/div.",
+            "0.5 s/div.",
+            "1 s/div.",
+            "2 s/div.",
+            "5 s/div.",
+            "10 s/div."
         ])
         time_layout.addWidget(time_combo)
         bottom_layout.addWidget(time_group, stretch=1)
